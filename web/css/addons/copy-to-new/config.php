db948f928f4b9b364b5af89a49674db4982c15c6
Code Space Services <47936032+codespaceservices@users.noreply.github.com>
2025-03-18 11:42:07 +0000

    added copy to new config for template

--- web/css/addons/copy-to-new/config.php
+++ web/css/addons/copy-to-new/config.php
@@ -7,7 +7,7 @@
     // Names of the templates to use this addon with (use '|' to separate multiple templates) e.g.
     // $cfg['tpls'] = 'products.php | specs.php | users/profile.php';
 
-    $cfg['tpls'] = '';
+    $cfg['tpls'] = 'companies.php | services.php | plans.php';
 
     // Button
     $cfg['btn_text'] = 'Copy to New';

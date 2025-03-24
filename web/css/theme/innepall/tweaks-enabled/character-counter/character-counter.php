8ea27f6cb7033924c0bc1bb75eb21b3fa8b1f0aa
Code Space Services <mail.codespace@gmail.com>
2025-03-24 12:42:55 +0545

    Added Character Counter

---
+++ web/css/theme/innepall/tweaks-enabled/character-counter/character-counter.php
@@ -0,0 +1,17 @@
+<?php
+    
+    if ( !defined('K_ENGINE_DIR') ) die(); // cannot be loaded directly
+    
+    class CharacterCounter{
+        static function character_counter( $params, $node ){
+            $params = trim(preg_replace('/\s\s+/', ' ', $node->children[0]->text));
+
+            $script = 'var my_counters=['.$params.'];';
+            $script .= file_get_contents(K_ADMIN_URL . 'addons/character-counter/js/characterCounter.min.js');
+        
+            return $script;      
+        }        
+    }
+$FUNCS->register_tag( 'character_counter', array('CharacterCounter', 'character_counter') );
+//TODO: repeatable 'remove row' breaks 'add row' listener
+//Maybe wait for expected update to repeatable regions, though
